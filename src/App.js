f91c1186c6824b6537561c5fa884a33a4af80802
Monika Gusain <gusainm@google.com>
2024-02-05 07:29:20 +0530

    popup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
+import React,{ useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar.js';
-// import TextForm from './components/TextForm';
-// import About from './components/About';
-import Filter from './components/filter'
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+ //import About from './components/About';
+//impogit Filter from './components/filter'
 
 function App() {
+
+  const[mode, setMode] = useState('light')  //weather dark mode is enables or not
+  
+
+  const[alert, setAlert] = useState(null)
+
+  const showAlert = (message, type) =>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+  }
+
+  const toggleMode = () =>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor="black";
+      document.body.style.color="white";
+      showAlert('Dark mode has been enabled', 'success');
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = "white";
+      document.body.style.color="black";
+      showAlert('Light mode has been enabled', 'success');
+    }
+    
+  }
   return (
    <>
-    <Navbar title="TextUtil" About="About"/>
-
+    <Navbar title="TextUtil" About="About" mode={mode} toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
     <div className="container my-3">
-      {/* <About/>
-      <TextForm/>
-    <TextForm heading="Enter the text to analyze"/> */}
-    <Filter/>
+      {/* <About/> */}
+        <TextForm/>
+      {/* <Filter/> */}
     </div>
    </>
   );
